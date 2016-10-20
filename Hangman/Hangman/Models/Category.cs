e7ecbf71ce62ef708d7fb6017b060cf5bc74a33f
kiranhsgithub <hskiran.bms@gmail.com>
2016-10-20 12:28:49 -0400

    added application db context

---
+++ Hangman/Hangman/Models/Category.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace Hangman.Models
+{
+    public class Category
+    {
+
+        public int Id { get; set; }
+
+        public string Level { get; set; }
+
+        public List<String> Words { get; set; }
+
+        public string SelectedRandomWord { get; set; }
+
+    }
+}
